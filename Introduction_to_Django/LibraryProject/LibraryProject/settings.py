7c4fa713bbf1ac20804cf02a8725cfb945b274a3
oussama-dot <eloussamafouah@gmail.com>
2025-08-10 15:05:44 +0100

    all

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',"bookshelf"
+    'django.contrib.staticfiles',"bookshelf","relationship_app"
 ]
 
 MIDDLEWARE = [
