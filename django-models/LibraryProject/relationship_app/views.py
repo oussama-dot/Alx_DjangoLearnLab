e964c28737bdb176b2319fac3fc2798a17250eac
oussama-dot <eloussamafouah@gmail.com>
2025-08-11 10:59:46 +0100

    yoo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,3 +7,8 @@ from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
