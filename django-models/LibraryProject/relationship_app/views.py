4e0845cc48200cc647e8e11bc273b84007e5fa2c
oussama-dot <eloussamafouah@gmail.com>
2025-08-11 11:24:39 +0100

    yoo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm  # <-- This line is required
+from django.contrib.auth.forms import UserCreationForm  # <-- This line is required
 from django.contrib.auth.decorators import login_required
 
 
