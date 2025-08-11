4594b1fe716f9fbffd21091a3e9bd6f493294aa1
oussama-dot <eloussamafouah@gmail.com>
2025-08-11 10:57:55 +0100

    yoo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,4 +6,4 @@ from .models import Book, Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
