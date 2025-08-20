c3506678d7cf11c7646ab654b52824767d099db8
oussama-dot <eloussamafouah@gmail.com>
2025-08-20 16:31:05 +0100

    ttyy

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+# api/views.py
+from rest_framework import generics, viewsets
+from .models import Book
+from .serializers import BookSerializer
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -6,3 +10,6 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
