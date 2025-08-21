12f4a775348cc5445eaf2ac82644e5839da7b70f
oussama-dot <eloussamafouah@gmail.com>
2025-08-21 13:17:32 +0100

    ff

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,31 +7,28 @@ from .serializers import BookSerializer
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # Public
+    permission_classes = [AllowAny]  # Public access
 
 # Retrieve details of a specific book
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # Public
+    permission_classes = [AllowAny]  # Public access
 
 # Create a new book (only authenticated users)
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Auth required
+    permission_classes = [IsAuthenticated]  # Auth required
 
 # Update an existing book (only authenticated users)
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Auth required
+    permission_classes = [IsAuthenticated]  # Auth required
 
 # Delete a book (only authenticated users)
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Auth required
-    def perform_create(self, serializer):
-        # You could assign additional data here
-        serializer.save()
+    permission_classes = [IsAuthenticated]  # Auth required
